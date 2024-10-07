cc9ebcf04ed27b4a7f290b8cc1d77e76da6f190b
Ryan Christopher <rechristopher99@gmail.com>
2024-10-07 00:30:29 -0400

    Added typescript to project and interactive map to landing

--- Frontend/terrier-map/app/page.js
+++ Frontend/terrier-map/app/page.js
@@ -1,22 +1,31 @@
 import Image from "next/image";
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../components/Map"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-10 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-        <h1>TerrierMap</h1>
+        <h1 className="font-['Caprasimo'] text-center text-[100px] ml-auto mr-auto inline-block">TerrierMap</h1>
         <Image
       src="/Logo.png"
-      width={300}
-      height={300}
+      width={200}
+      height={200}
       alt="TerrierMap logo of a Boston Terrier dog in front of a topographical map of Boston."
+      className="ml-auto mr-auto"
     />
+
+        <Map />
+
 
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 
-        <h3>By: Ryan Christopher, Jasmine Hughes, Siddhraj Parmar, and Misael Gared</h3>
+        <h3 className="font-['Barlow']">By: Ryan Christopher, Jasmine Hughes, Siddhraj Parmar, and Misael Gared</h3>
 
       </footer>
     </div>
