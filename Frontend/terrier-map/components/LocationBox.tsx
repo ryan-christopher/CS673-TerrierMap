9b41d615bce6ef466a07518ce3b428ca80681fcf
Siddhrajsinh Parmar <apple@Apples-MacBook-Pro.local>
2024-10-22 15:26:08 -0400

    Updated LocationBox component to handle geolocation and error messages

--- Frontend/terrier-map/components/LocationBox.tsx
+++ Frontend/terrier-map/components/LocationBox.tsx
@@ -1,10 +1,13 @@
-
 'use client';
 
 import { useState } from 'react';
 
-const LocationBox = ({ setLocation }: { setLocation: (location: { latitude: number, longitude: number }) => void }) => {
-    //   const [location, setLocation] = useState({ latitude: '', longitude: '' });
+interface Location {
+  latitude: number;
+  longitude: number;
+}
+
+const LocationBox = ({ location, setLocation }: { location: Location | null, setLocation: (location: Location) => void }) => {
   const [errorMessage, setErrorMessage] = useState('');
 
   const getLocation = () => {
@@ -16,7 +19,7 @@ const LocationBox = ({ setLocation }: { setLocation: (location: { latitude: numb
             longitude: position.coords.longitude,
           });
         },
-        (error) => {
+        () => {
           setErrorMessage("Unable to retrieve your location");
         }
       );
@@ -27,8 +30,10 @@ const LocationBox = ({ setLocation }: { setLocation: (location: { latitude: numb
 
   return (
     <div>
-      <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700" onClick={getLocation}>Get Current Location</button>
-      {location.latitude && location.longitude ? (
+      <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700" onClick={getLocation}>
+        Get Current Location
+      </button>
+      {location ? (
         <p>
           Latitude: {location.latitude}, Longitude: {location.longitude}
         </p>
